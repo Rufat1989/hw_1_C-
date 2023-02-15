f3e7de3839e10baa67db6cb61cd10b3207a9d079
Rufat Karimov <ruffat_karimov@mail.ru>
2023-02-15 18:33:06 +0600

    Задача 2

---
+++ Task_2/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите число: ");
+int x = int.Parse(Console.ReadLine()!);
+
+if(x % 2 == 0)
+{
+    Console.Write("Да, число " + x + " четное");
+}
+else
+{
+    Console.Write("Нет,число " + x + " нечетное");
+}
