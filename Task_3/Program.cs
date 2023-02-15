2faa35f222f37590dc3258df4af2e0c48a7f3e6a
Rufat Karimov <ruffat_karimov@mail.ru>
2023-02-15 19:35:26 +0600

    Задача 3

---
+++ Task_3/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите число: ");
+int n = int.Parse(Console.ReadLine()!);
+int x = 2;
+
+while(x <= n)
+{
+    Console.Write(x);
+
+    if(x < n-1)
+    {
+        Console.Write(",");
+    }
+    x = x + 2;
+}
